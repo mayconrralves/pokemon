334d6676a124848727952f6a9fc41dbf765c4b1c
maycon r r alves <mayconrralves@yahoo.com.br>
2021-11-16 08:21:30 -0300

    Add global style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React  from "react";
 import {BrowserRouter as Router}  from 'react-router-dom';
 import MyRoutes from './MyRoutes';
-
+import GlobalStyles from './styles';
 function App() {
   return (
     <Router>
       <MyRoutes/>
+      <GlobalStyles />
     </Router>
   ) 
 }
---
+++ src/styles.js
@@ -0,0 +1,16 @@
+  
+import { createGlobalStyle  } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+	
+	*{
+		margin: 0;
+		box-sizing: border-box;
+	}
+	html, body {
+		width: 100vw;
+		height: 100vh;	
+	}
+`;
+
+export default GlobalStyle;
